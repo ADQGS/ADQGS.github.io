91929e9a1f8d7cfaf4a3b0299188d9847ba5ff75
Bragatte <marcelobragatte@gmail.com>
2023-02-16 22:29:37 -0300

    Criação do projeto livro on-line

---
+++ _quarto.R
@@ -0,0 +1,45 @@
+github-card: true
+chapters:
+    - index.qmd
+- setup.qmd
+- python_r.qmd
+- references.qmd
+code_fold: true
+code-tools:
+    source: repo
+toggle: false
+caption: none
+
+
+reader-mode: true
+
+page-footer:
+    left: |
+        written by Bert Gollnick
+    right: |
+        This book was built with <a href="https://quarto.org/">Quarto</a>.
+cover-image: cover.png
+site-url: https://datasciencehamburg.github.io/QuartoBookSample/
+    repo-url: https://github.com/DataScienceHamburg/QuartoBookSample
+
+bibliography: references.bib
+
+format:
+    html:
+        code-link: true
+        code-line-numbers: true
+        embed-resources: true
+        theme:
+            light: flatly
+            dark: darkly
+    pdf:
+        documentclass: scrreprt
+
+
+
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security

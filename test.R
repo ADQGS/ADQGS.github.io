6bbaaa4d52731cf294a01cee06b0193a5c7f63ed
Bragatte <marcelobragatte@gmail.com>
2023-03-10 18:36:41 -0300

    updates

---
+++ test.R
@@ -0,0 +1,16 @@
+
+
+1.  Velocidade
+2.  Variabilidade de aplicação
+3.  Complexidade de uso
+
+Pacotes essenciais para manipulação - vulgo limpeza dos dados precisamos manipular os dados e transformar em informação
+
+## dplyr
+
+-   funções:
+    -   filter() \# filtrar
+-   group_by() \# agrupar
+-   summarize() \# correlacionar
+-   mutate() \# modificar
+-   arrange() \# arranjar os dados

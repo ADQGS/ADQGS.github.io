2819d88b5f27d4a6cb24eff02f5526f50fff4f66
Bragatte <marcelobragatte@gmail.com>
2023-04-08 18:31:34 -0300

    updateR

---
+++ aulas/AulaExtra_chatgpt.R
@@ -0,0 +1,9 @@
+library(openai)
+
+Sys.setenv(openai_organization_id = {your_organization_id})
+Sys.setenv(openai_secret_key = {your_secret_key})
+
+
+
+## Ref
+## https://github.com/samterfa/openai
